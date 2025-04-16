f3bf6bb0cfca6078243e1bbcc2458f3c0a6534f2
Denise Nanni <denise.nanni1@gmail.com>
2025-04-16 13:10:56 +0200

    project setup

---
+++ FindTheDependencies/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.github.javaparser:javaparser-core:3.26.4")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
