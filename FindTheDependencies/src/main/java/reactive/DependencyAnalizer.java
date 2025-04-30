d4a71598a1a7aecf4fceb243fbd0e022667962e4
Denise Nanni <denise.nanni1@gmail.com>
2025-04-30 12:00:45 +0200

    initial retrieve of dependencies in packages

--- FindTheDependencies/src/main/java/reactive/DependencyAnalizer.java
+++ FindTheDependencies/src/main/java/reactive/DependencyAnalizer.java
@@ -1,4 +1,56 @@
 package reactive;
 
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+
 public class DependencyAnalizer {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Dependency Analyzer");
+        frame.setSize(1000, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        JList<String> lista = new JList<>(listModel);
+        JScrollPane scrollPane = new JScrollPane(lista);
+        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+        DependencyAnalizerReactive depReactive = new DependencyAnalizerReactive();
+
+        Path root = Path.of("src\\main\\java");
+
+        JButton analyzeButton = new JButton("Analyze");
+        analyzeButton.addActionListener(e -> {
+            listModel.clear();
+            try (Stream<Path> files = Files.walk(root)) {
+                Observable.fromIterable(files
+                                .filter(p -> p.toString().endsWith(".java"))
+                                .toList())
+                        .flatMap(path ->
+                                depReactive.parseClassDependenciesIncrementally(path)
+                                        .subscribeOn(Schedulers.io())
+                        )
+                        .observeOn(Schedulers.trampoline())
+                        .subscribe(
+                                riga -> SwingUtilities.invokeLater(() -> listModel.addElement(riga)),
+                                Throwable::printStackTrace,
+                                () -> System.out.println("Analisi completata.")
+                        );
+
+            } catch (IOException exc) {
+                exc.printStackTrace();
+            }
+        });
+
+        frame.add(analyzeButton, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+    }
 }
---
+++ FindTheDependencies/src/main/java/reactive/DependencyAnalizerReactive.java
@@ -0,0 +1,39 @@
+package reactive;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.subjects.PublishSubject;
+
+import java.nio.file.Path;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class DependencyAnalizerReactive {
+
+//    public record ClassDependency(String className, String packageName, Set<String> usedTypes) {}
+
+    public Observable<String> parseClassDependenciesIncrementally(Path javaFile) {
+        return Observable.create(emitter -> {
+            try {
+                CompilationUnit cu = StaticJavaParser.parse(javaFile);
+
+                Set<String> dep = cu.findAll(ClassOrInterfaceType.class).stream()
+                        .map(ClassOrInterfaceType::getNameAsString)
+                        .collect(Collectors.toSet());
+                dep.addAll(cu.findAll(ObjectCreationExpr.class).stream()
+                        .map(ObjectCreationExpr::getTypeAsString)
+                        .collect(Collectors.toSet()));
+                dep.forEach(emitter::onNext);
+
+                emitter.onComplete();
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
+        });
+    }
+}
